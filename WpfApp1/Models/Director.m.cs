bf75c78aa33539bc33fa8b818b4e241f763c96ea
tlvu21@clc.fitus.edu.vn <vucongleo@gmail.com>
2024-03-01 23:13:56 +0700

    Build: create models.

---
+++ WpfApp1/Models/Director.m.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class Director
+    {
+        public string Id { get; set; }
+        public string? Name { get; set; }
+        // image can change datatype
+        public string? Image { get; set; }
+        public string? Story { get; set; }
+    }
+}
---
+++ WpfApp1/Models/Movie.m.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    class Movie
+    {
+        public string Id { get; set; }
+        // ? can be assign null
+        public string? IdGener { get; set; }
+        public string? Release { get; set; }
+        public string? Rating { get; set; }
+        public string? Poster { get; set; }
+        public string? IdStar { get; set; }
+        public string? IdDirector { get; set; }
+        public string? Certification { get; set; }
+    }
+}
