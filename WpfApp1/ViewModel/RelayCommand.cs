cf8ecb46ea2cd5a0e8e33340d950640359414fff
tlvu21@clc.fitus.edu.vn <vucongleo@gmail.com>
2024-03-12 23:56:38 +0700

    Build: Pagination in search, install Fody to binding data shorten.

---
+++ WpfApp1/ViewModel/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        private Action _execute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true; //
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke();
+        }
+    }
+}
--- WpfApp1/ViewModel/SearchViewModel.cs
+++ WpfApp1/ViewModel/SearchViewModel.cs
@@ -1,36 +1,64 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WpfApp1.Models;
 
 namespace WpfApp1.ViewModel
 {
     class SearchViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
         public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
+        public int CurrentPage { get; set; }
+        private int _pageSize = 2;
+        private int _totalMovies = 8;
+        private int _quantityPage = 0;
 
+        // get from database
+        public ArrayList a = new ArrayList();
         public SearchViewModel()
         {
-            // Populate the People collection with some sample data
-            Movies.Add(new Movie { Name = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda1", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda2", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda3", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda4", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda5", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda6", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-            Movies.Add(new Movie { Name = "Kungfu panda7", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
-        }
+            CurrentPage = 0;
+            _quantityPage = _totalMovies / _pageSize;
+            // test when dont have database
+            a.Add(new Movie { Name = "0", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda1", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda2", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda3", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda4", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda5", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda6", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            a.Add(new Movie { Name = "Kungfu panda7", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
 
-        // Implement INotifyPropertyChanged interface
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
+            updateMovie(1);
+        }
+        public ICommand PreviousPageCommand => new RelayCommand(Previous);
+        public ICommand NextPageCommand => new RelayCommand(next);
+        private void next()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            CurrentPage++;
+            CurrentPage = CurrentPage % _quantityPage;
+            updateMovie(CurrentPage);
+        }
+        private void Previous()
+        {
+            CurrentPage--;
+            CurrentPage = (CurrentPage + _quantityPage) % _quantityPage;
+            updateMovie(CurrentPage);
+        }
+        private void updateMovie(int curPage)
+        {
+            int startIndex = curPage > 0 ? curPage * _pageSize : 0;
+            int endIndex = Math.Min(startIndex + _pageSize, _totalMovies);
+            Movies.Clear();
+            for (int i = startIndex; i < endIndex; i++) Movies.Add((Movie)a[i]);
         }
     }
 }
