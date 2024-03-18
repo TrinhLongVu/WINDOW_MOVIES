c7f5809b7de6ed1d2c1741e62740dc0d4a9969cd
SimonGin <tt26419@gmail.com>
2024-03-18 14:13:27 +0700

    Build: Add 1 more layer to the navigation system

---
+++ WpfApp1/Views/Dashboard.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace WpfApp1.Views
+{
+    public partial class Dashboard : Page
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminLogOut(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Login());
+        }
+    }
+}
--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -16,9 +16,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
