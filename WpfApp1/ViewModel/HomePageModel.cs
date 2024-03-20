d3974e38a6581149572465834706deba82196fa6
pross1312 <dangvinhtuong12@gmail.com>
2024-03-20 15:26:49 +0700

    Feature: Fetch data from DB to display in 'Home'

--- WpfApp1/ViewModel/HomePageModel.cs
+++ WpfApp1/ViewModel/HomePageModel.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp1.Database;
 using WpfApp1.Models;
 
 namespace WpfApp1.ViewModel
 {
     class HomePageModel
     {
-        public MovieCarousel TestCarousel { get; set; } = new MovieCarousel(4);
+        public MovieCarousel TestCarousel { get; set; }
         public HomePageModel() {
+            ArrayList movies = new MovieDB().GetAllMovie();
+            TestCarousel = new MovieCarousel(new List<Movie>(movies.ToArray(typeof(Movie)) as Movie[]), 4);
         }
     }
 }
