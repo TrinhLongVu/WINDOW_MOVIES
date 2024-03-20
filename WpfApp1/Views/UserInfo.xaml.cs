ad13bd51b45ccf74f22537deb81bf289e71ec463
SimonGin <tt26419@gmail.com>
2024-03-20 13:21:45 +0700

    Build: Information Window For User

---
+++ WpfApp1/Views/UserInfo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for UserInfo.xaml
+    /// </summary>
+    public partial class UserInfo : Window
+    {
+        public UserInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
