f2b8138c338dba64874c5e095ad2f1a27d4fd8e6
SimonGin <tt26419@gmail.com>
2024-03-20 08:59:06 +0700

    Feature: Add Movies List to Admin Dashboard

--- WpfApp1/Views/Dashboard.xaml.cs
+++ WpfApp1/Views/Dashboard.xaml.cs
@@ -1,15 +1,42 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Navigation;
+using WpfApp1.Models;
 
 namespace WpfApp1.Views
 {
+    class AdminMovieManageViewModel : INotifyPropertyChanged
+    {
+        public AdminMovieManageViewModel()
+        {
+            Movies.Add(new Movie { Title = "Mai(T18) wda da wad adaw dadw", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+            Movies.Add(new Movie { Title = "Mai(T18)", Poster = "https://api-website.cinestar.com.vn/media/wysiwyg/Posters/03-2024/sang-den-poster.jpg" });
+        }
+
+        public ObservableCollection<Movie> Movies { get; set; } = new ObservableCollection<Movie>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
     public partial class Dashboard : Page
     {
         public Dashboard()
         {
             InitializeComponent();
+            DataContext = new AdminMovieManageViewModel();
         }
 
         private void AdminInitStatistic(object sender, RoutedEventArgs e)
@@ -22,7 +49,7 @@ namespace WpfApp1.Views
             Button focusedButton = sender as Button;
             if (focusedButton != null && focusedButton.IsFocused)
             {
-                MessageBox.Show("Button is focused.");
+                //MessageBox.Show("Button is focused.");
             }
         }
 
