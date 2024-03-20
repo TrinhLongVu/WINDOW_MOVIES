5295f82bc33727b0ac03a02354473d2e5125c226
pross1312 <dangvinhtuong12@gmail.com>
2024-03-20 15:55:57 +0700

    Feature: Home page UI

---
+++ WpfApp1/Views/PlaceholderConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace WpfApp1.Views
+{
+    class PlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = false;
+            if (value is string)
+            {
+                flag = ((string)value).Trim() == "";
+            }
+            return flag ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
