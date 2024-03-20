7d35c6e1dc188ac58d2716a4079e8f6bd012f743
tlvu21@clc.fitus.edu.vn <vucongleo@gmail.com>
2024-03-20 22:03:16 +0700

    Fix: handle conflix.

--- WpfApp1/Views/UserPage.xaml.cs
+++ WpfApp1/Views/UserPage.xaml.cs
@@ -15,12 +15,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp1.Views
 {
-<<<<<<< HEAD
-    /// <summary>
-    /// Interaction logic for UserPage.xaml
-    /// </summary>
-=======
->>>>>>> 34a36b4e1be34170753419015bf638d091dbf8ff
     public partial class UserPage : Page
     {
         public UserPage()
@@ -29,21 +23,17 @@ namespace WpfApp1.Views
         }
 
         private void loginLink_Click(object sender, RoutedEventArgs e) {
-<<<<<<< HEAD
-            Body.NavigationService.Navigate(new Login());
+            UserBody.NavigationService.Navigate(new Login());
         }
 
         private void SearchBtn(object sender, RoutedEventArgs e)
         {
-            Body.NavigationService.Navigate(new Search(SearchBox.Text));
-=======
-            UserBody.NavigationService.Navigate(new Login());
+            UserBody.NavigationService.Navigate(new Search(SearchBox.Text));
         }
 
         private void navigateHome(object sender, RoutedEventArgs e)
         {
             UserBody.NavigationService.Navigate(new Home());
->>>>>>> 34a36b4e1be34170753419015bf638d091dbf8ff
         }
     }
 }
