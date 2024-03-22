b4c023a0fdab0f32698bf21784d8c87465cb253a
SimonGin <tt26419@gmail.com>
2024-03-22 15:22:25 +0700

    Update: Styling book buttons

--- WpfApp1/Views/Home.xaml.cs
+++ WpfApp1/Views/Home.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 using WpfApp1.ViewModel;
 
 namespace WpfApp1.Views {
