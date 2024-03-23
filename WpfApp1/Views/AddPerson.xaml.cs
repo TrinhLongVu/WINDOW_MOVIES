7e80f94646d8467a55d6b97e29891580b654cdff
SimonGin <tt26419@gmail.com>
2024-03-23 11:11:39 +0700

    Build: UI for add person window

---
+++ WpfApp1/Views/AddPerson.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for AddPerson.xaml
+    /// </summary>
+    public partial class AddPerson : Window
+    {
+        public AddPerson(String type)
+        {
+            InitializeComponent();
+            if (type == "director")
+            {
+                Title = "Add a director";
+            }
+            else if (type == "cast")
+            {
+                Title = "Add a cast";
+            }
+        }
+
+        private void imgUrlChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                string text = textBox.Text;
+                try
+                {
+                    BitmapImage bitmapImage = new BitmapImage(new Uri(text));
+                    AvatarPreview.ImageSource = bitmapImage;
+                }
+                catch (Exception ex)
+                {
+                    AvatarPreview.ImageSource = null;
+                }
+            }
+        }
+    }
+}
